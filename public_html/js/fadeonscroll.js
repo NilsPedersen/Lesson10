cb6bd16bc204422f007fa51cfce8fa984dac0b42
NilsPedersen <info@beziercurve.se>
2018-10-03 13:13:02 +0200

    Removed semicolon

--- public_html/js/fadeonscroll.js
+++ public_html/js/fadeonscroll.js
@@ -7,30 +7,31 @@ function setAlpha(alpha) {
 } 
 
 
-function calcAlpha(); {
-   // define value for alpha
-   let alpha = 0;
-   // define variable for how far user scrolls from the top of the document
-   let scroll = $(document).scrollTop();
-   // at what pixel the fade should start.
-   const fadeStart = 100;
-   // at what pixel the fade should end.
-   const fadeStop = 200;
-   // Distance of fade.
-   const fadeLength = fadeStop - fadeStart;
+function calcAlpha() {
+  // define value for alpha
+  let alpha = 0;
+  // define variable for how far user scrolls from the top of the document
+  let scroll = $(document).scrollTop();
+  // at what pixel the fade should start.
+  const fadeStart = 100;
+  // at what pixel the fade should end.
+  const fadeStop = 200;
+  // Distance of fade.
+  const fadeLength = fadeStop - fadeStart;
 
 
    // There are three cases: betwen 0 and fadestart, fadestart and fadestop, and from fadeStop.
-   if (scroll < fadeStart) {
-     alpha = 0;
-   } else if (scroll < fadeStop) {
-     alpha = (scroll - fadeStart) / fadeLength;
-   } else {
-     alpha = 1;
-   }
+  if (scroll < fadeStart) {
+    alpha = 0;
+  } else if (scroll < fadeStop) {
+    alpha = (scroll - fadeStart) / fadeLength;
+  } else {
+    alpha = 1;
+  }
 
-   // return the alpha value after the IF code converts the input.
-   return alpha;
+  // return the alpha value after the IF code converts the input.
+  return alpha;
+
 }
 
 // when document scrolls - run the setAlpha function with the value from calcAlpha.
